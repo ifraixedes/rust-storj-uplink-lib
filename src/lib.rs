45517d32bd8eb63c2581d3542445a5a847fa1905
Ivan Fraixedes <ivan@fraixed.es>
2021-08-08 21:03:16 +0200

    feat: Implement base types & Uplink 'access'
    Implement the Uplink 'access', its bound types, and create related types
    which are needed for creating it.
    
    This commit also implements some base types to be used by the several
    types that the crate is going to expose:
    
    * A specific error type which will be returned as an error by any
      implemented type for returning homogenized errors and ease the
      consumers to handle them appropriately.
    * A helper function to convert strings to C strings.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,28 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+//! Storj DCS Uplink idiomatic and safe Rust bindings.
+
+#![deny(missing_docs)]
+
+pub(crate) mod access;
+pub(crate) mod encryption_key;
+pub mod error;
+pub(crate) mod helpers;
+pub(crate) mod project;
+
+pub use access::Access;
+pub use access::Permission;
+pub use access::SharePrefix;
+pub use encryption_key::EncryptionKey;
+pub use error::Error;
+pub use project::Project;
+
+/// An interface for ensuring that an instance of type returned by the
+/// underlying c-binding is correct in terms that it doesn't violate its own
+/// rules.
+/// For example a UplinkAccessResult struct has 2 fields which are 2 pointers,
+/// one is the access and the other is an error, always one and only one can be
+/// NULL.
+trait Ensurer {
+    /// Checks that the instance is correct according its own rules and it
+    /// returns itself, otherwise it panics.
+    fn ensure(&self) -> &Self;
 }
